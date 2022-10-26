537786fdd7819fad244dbad94572a084ed180563
addis-hail <addishailegnaw@yahoo.com>
2022-10-26 21:18:10 +0300

    react components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+//import ReactDOM from 'react-dom';
+import './index.css';
 
-import WeatherCard from './components/WeatherCard/component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <WeatherCard temp={3} condition = "Clear" city = "Sydney" country="AU"/>
-      <WeatherCard temp={20} condition = "Clouds" city = "Melbourne" country="AU"/>
-      <WeatherCard temp={40} condition = "Clear" city = "London" country="GB"/>
-    </div>
-  );
-}
+function App(){
+// var TodoComponent = React.createClass({
+//   function(){
+    return(
+            <h1>Helloooo</h1>
+    );
+  }
+//ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <WeatherCard temp={3} condition = "Clear" city = "Sydney" country="AU"/>
+//       <WeatherCard temp={20} condition = "Clouds" city = "Melbourne" country="AU"/>
+//       <WeatherCard temp={40} condition = "Clear" city = "London" country="GB"/> */}
+//     </div>
+//   );
+// }
+// // put component into html page
 
 export default App;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -14,4 +14,29 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
+// //Create component
+// var TodoComponent = React.createClass({
+//   render: function(){
+//     return(
+//       <h1>Helloooo</h1>
+//     );
+//   }
+// });
+
+// // put component into html page
+// ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+
+
+
+
+
+
+
+
+
+
+
+
