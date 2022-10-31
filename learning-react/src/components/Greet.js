1204db7debbaa7f4631b7102f338f0af0b65aaec
addis-hail <addishailegnaw@yahoo.com>
2022-10-31 08:15:40 +0300

    class component

--- learning-react/src/components/Greet.js
+++ learning-react/src/components/Greet.js
@@ -3,6 +3,6 @@ import React from "react";
 // function Greet(){
 //     return <h1>Hello there</h1>
 // }
-export const Greet = () => <h1>Hellooooo there</h1>
+const Greet = () => <h1>Hellooooo there</h1>
 
-// export default Greet;
+export default Greet;
