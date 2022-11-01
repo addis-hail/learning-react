e7fa79326eaa1a0faef7c92ed4301f7c0c49d201
addis-hail <addishailegnaw@yahoo.com>
2022-11-01 19:51:57 +0300

    props

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -7,9 +7,17 @@ import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <Welcome /> */}
-      <Hello />
+      <Greet name =  "Bruce" heroName = "Batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name = "Clark" heroName = "Superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name = "Diana" heroName = "Wondar Woman"/>
+      <Welcome name =  "Bruce" heroName = "Batman"/>
+      <Welcome name = "Clark" heroName = "Superman"/>
+      <Welcome name = "Clark" heroName = "Superman"/>
+      {/* <Hello /> */}
     </div>
   );
 }
--- learning-react/src/components/Greet.js
+++ learning-react/src/components/Greet.js
@@ -3,6 +3,16 @@ import React from "react";
 // function Greet(){
 //     return <h1>Hello there</h1>
 // }
-const Greet = () => <h1>Hellooooo there</h1>
+const Greet = (props) => {
+    console.log(props)
+    return ( 
+        <div>
+            <h1>Hellooooo {props.name} a.k.a {props.heroName}
+            </h1>
+            {props.children}
+        </div>
+        
+    ) 
+}
 
 export default Greet;
