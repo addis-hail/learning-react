e98e9eaa047129a67faa82f35737da687a7e0f53
addis-hail <addishailegnaw@yahoo.com>
2022-11-06 16:50:05 +0300

    conditional rendering

---
+++ learning-react/src/components/UserGreeting.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         isLoggedIn: true
+      }
+    }
+  render() {
+           return this.state.isLoggedIn && <div>Welcome stranger</div>
+    
+        // return (
+        //     this.state.isLoggedIn ?(
+        //         <div>Welcome stranger</div>
+        //     ): (
+        //         <div>Welcome guest</div>
+        //     )
+        // )
+    //    let message
+    //    if (this.state.isLoggedIn) {
+    //     message = <div>Welcome stranger</div>
+    //    } else {
+    //     message = <div>Welcome guest</div>
+    //    }
+    //    return <div>{message}</div>
+    // if (this.state.isLoggedIn) {
+    //     return (
+    //         <div>Welcome stranger</div>
+    //     )
+    // } else {
+    //     return(
+    //         <div>Welcome guest</div>
+    //     )
+    // }
+    // return (
+    //     <div>
+    //         <div>Welcome stranger</div>
+    //         <div>Welcome guest</div>
+    //     </div>
+      
+    // )
+  }
+}
+
+export default UserGreeting
